f0ae0e64972edd04066e44feeedd9f95d9645d12
Alaor Resende <alaor.resende@gmail.com>
2020-01-18 20:05:20 -0300

    Add vegetation-cover endpoint.

--- api/api.py
+++ api/api.py
@@ -6,10 +6,10 @@ app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
-    return vegetation_description()
+    return vegetation_cover()
 
 @app.route('/vegetation-cover', methods=['GET'])
-def vegetation_description():
+def vegetation_cover():
     return "<h1>GeoTIFF's Information API</h1><p>Calculates vegetation cover and some geographical information for a given file in the server (preexisting).</p>"
 
 app.run()
