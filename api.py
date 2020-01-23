d2c82accc2d501782091111dcbad9124b43f9181
eteg.alaor <alaor.resende@eteg.com.br>
2020-01-23 15:01:40 -0300

    #2 - Heroku env finished and ready to merge at master.

--- api.py
+++ api.py
@@ -1,16 +1,17 @@
-from connexion.resolver import RestyResolver
 import connexion
 import os
 
 app = connexion.App(__name__, specification_dir='./')
 app.add_api("swagger_api.yml")
 
+
 @app.route('/')
 def home():
     return u"<h1>GeoTIFF's Information API</h1><p>Calculates vegetation cover and some geographical " \
            u"information for a given file in the server (preexisting).</p>" \
-           "<p>The information is available on https://alaor-strdr.herokuapp.com/vegetation-cover " \
-           "or if you're running local at localhost:5000/vegetation-cover.</p>"
+           "<p>The information is available <a href='https://alaor-strdr.herokuapp.com/vegetation-cover'>HERE</a> " \
+           "or if you're running locally you can also access at <a href='http://localhost:5000/vegetation-cover'>this link</a>.</p>"
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
