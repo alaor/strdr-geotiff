9a3449597458006e8adb44a28b1c4f13759553c5
eteg.alaor <alaor.resende@eteg.com.br>
2020-01-23 14:38:45 -0300

    Adjusting message on main page.

--- api.py
+++ api.py
@@ -9,7 +9,8 @@ app.add_api("swagger_api.yml")
 def home():
     return u"<h1>GeoTIFF's Information API</h1><p>Calculates vegetation cover and some geographical " \
            u"information for a given file in the server (preexisting).</p>" \
-           "<p>The information is available on localhost:5000/vegetation-cover.</p>"
+           "<p>The information is available on https://alaor-strdr.herokuapp.com/vegetation-cover " \
+           "or if you're running local at localhost:5000/vegetation-cover.</p>"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
