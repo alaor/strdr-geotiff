47de2f0c89dc95bb6bc5677204e3d7db79b71580
Alaor Resende <alaor.resende@gmail.com>
2020-01-25 15:15:46 -0300

    Using rasterio lnglat to get lnglat.

--- geotiffinfo.py
+++ geotiffinfo.py
@@ -1,4 +1,6 @@
 import rasterio
+import rasterio.features
+import rasterio.warp
 import numpy as np
 from affine import Affine
 from pyproj import Proj, transform
@@ -10,23 +12,26 @@ dataset = rasterio.open(path)
 def centroid():
     # ref: https://github.com/EarthScientist/etc/blob/master/centroid_from_raster.py
     # T0 = r.affine  # not working, fix below, solution found at: https://github.com/mdbartos/pysheds/issues/13
-    T0 = dataset.transform  # upper-left pixel corner affine transform
-    p1 = Proj(dataset.crs)
-    A = dataset.read(1)  # pixel values
+    # T0 = dataset.transform  # upper-left pixel corner affine transform
+    # p1 = Proj(dataset.crs)
+    # A = dataset.read(1)  # pixel values
+    #
+    # # All rows and columns
+    # cols, rows = np.meshgrid(np.arange(A.shape[1]), np.arange(A.shape[0]))
+    # # Get affine transform for pixel centres
+    # T1 = T0 * Affine.translation(0.5, 0.5)
+    # # Function to convert pixel row/column index (from 0) to easting/northing at centre
+    # rc2en = lambda r, c: (c, r) * T1
+    # # All eastings and northings -- this is much faster than np.apply_along_axis
+    # eastings, northings = np.vectorize(rc2en, otypes=[np.float, np.float])(rows, cols)
+    # # Project all longitudes, latitudes as EPSG:4326
+    # longs, lats = transform(p1, p1.to_latlong(), eastings, northings)
+    # long = longs.sum() / longs.size
+    # lat = lats.sum() / lats.size
+    # return {"coordinates": [lat, long]}
 
-    # All rows and columns
-    cols, rows = np.meshgrid(np.arange(A.shape[1]), np.arange(A.shape[0]))
-    # Get affine transform for pixel centres
-    T1 = T0 * Affine.translation(0.5, 0.5)
-    # Function to convert pixel row/column index (from 0) to easting/northing at centre
-    rc2en = lambda r, c: (c, r) * T1
-    # All eastings and northings -- this is much faster than np.apply_along_axis
-    eastings, northings = np.vectorize(rc2en, otypes=[np.float, np.float])(rows, cols)
-    # Project all longitudes, latitudes as EPSG:4326
-    longs, lats = transform(p1, p1.to_latlong(), eastings, northings)
-    long = longs.sum() / longs.size
-    lat = lats.sum() / lats.size
-    return {"coordinates": [lat, long]}
+    # after reading more about rasterio library, found this method which returns the same value of the algorithm above.
+    return {"type":"Point", "coordinates": [dataset.lnglat()]}
 
 
 def vegetationCover():
